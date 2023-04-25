4bff0e8442d5135929558fa8fca27210ade49348
aabdulbasset <aabdulbasset5@gmail.com>
2023-04-25 05:44:29 +0200

    initialized database connection outside of lambda handler

---
+++ makeLink.py
@@ -0,0 +1,38 @@
+import json
+import os
+import random
+import string
+import psycopg2
+from urllib.parse import urlparse
+url = urlparse(os.environ['DATABASE_URL'])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port,
+    sslmode='require',
+    )
+
+def create_shorturl(event, context):
+
+    data = json.loads(event['body'])
+    original_url = data['url']
+    if 'slug' in data:
+        shorturl = data['slug']
+    else:
+        # Generate a random 6-character alphanumeric string
+        chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+        shorturl = ''.join(random.choices(chars, k=6))
+    cur = conn.cursor()
+    cur.execute("INSERT INTO urls (shorturl, url, hits, devices, locations) VALUES (%s, %s, %s, %s, %s)",
+                    (shorturl, original_url, 0, '{}', '{}'))
+    conn.commit()
+    # Return the short URL and its stats
+    stats = {'shorturl': shorturl, 'hits': 0, 'devices': {}, 'locations': {}}
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(stats)
+    }
